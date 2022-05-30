0b991ebd00202832367bef4eeed35baf417ac32e
Emmett Butler <emmett.butler321@gmail.com>
2022-05-29 18:43:59 -0700

    scale splashies

--- huhdremaster/SplashScene.swift
+++ huhdremaster/SplashScene.swift
@@ -4,6 +4,7 @@ import GameplayKit
 class SplashScene: SKScene {
     var startTime:Double = 0
     var phase:Int = 0
+    var scaleFactor: CGFloat = 1
     let logoBackground: SKSpriteNode = SKSpriteNode(imageNamed: "Logo_Cloud.png")
     var backgroundClouds: BackgroundClouds? = nil
     let mainLogo: SKSpriteNode = SKSpriteNode(imageNamed: "ASg_Logo.png")
@@ -16,6 +17,9 @@ class SplashScene: SKScene {
     override func didMove(to view: SKView) {
         anchorPoint = CGPoint(x:0.5, y:0.5)
         let displaySize: CGRect = UIScreen.main.bounds
+        if (UIDevice.current.userInterfaceIdiom == .pad) {
+            scaleFactor = 1.68
+        }
         
         let background = SKSpriteNode(imageNamed: "Splash_BG_clean.png")
         background.xScale = displaySize.width / background.size.width
@@ -26,17 +30,25 @@ class SplashScene: SKScene {
         backgroundClouds = BackgroundClouds(scene:self)
         
         logoBackground.zPosition = 20
+        logoBackground.xScale = scaleFactor
+        logoBackground.yScale = scaleFactor
         addChild(logoBackground)
         
         mainLogo.zPosition = 21
+        mainLogo.xScale = scaleFactor
+        mainLogo.yScale = scaleFactor
         addChild(mainLogo)
         
         namesBackground.alpha = 0
+        namesBackground.xScale = scaleFactor
+        namesBackground.yScale = scaleFactor
         namesBackground.zPosition = 20
         addChild(namesBackground)
         
         namesSprite.alpha = 0
         namesSprite.zPosition = 21
+        namesSprite.xScale = scaleFactor
+        namesSprite.yScale = scaleFactor
         addChild(namesSprite)
     }
     
